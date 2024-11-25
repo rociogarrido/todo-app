8be03a03a6515543f86fea4a639ef70897b0f10d
Rocio Garrido <rocioganon@gmail.com>
2024-11-25 11:48:47 +0100

    feat: add HTML structure for displaying and interacting with tasks

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrl: './todo-list.component.scss',
 })
 export class TodoListComponent {
   newTask: string = '';
-  tasks: { title: string, completed: boolean }[] = [];
+  tasks: { title: string; completed: boolean }[] = [];
 
   addTask() {
     if (this.newTask.trim()) {
@@ -21,5 +43,4 @@ export class TodoListComponent {
   removeTask(index: number) {
     this.tasks.splice(index, 1);
   }
-
 }
