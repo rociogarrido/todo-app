f30a5d7206d7ad12d8cdae5790c0e28931a047b7
Rocio Garrido <rocioganon@gmail.com>
2024-11-25 11:10:39 +0100

    feat: add task list functionality

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
+  newTask: string = '';
+  tasks: { title: string, completed: boolean }[] = [];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ title: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 
 }
